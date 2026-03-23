ba4054cf9a4bf41f34addea42a4724ce37c8b39d
srtsubham <somyar.tripathi@gmail.com>
2026-03-23 15:00:56 +0530

    Fixed scanning mechanism for database in model.

--- src/scanner.py
+++ src/scanner.py
@@ -49,7 +49,7 @@ def get_coords(path):
 
 def run():
     if not os.path.exists(DATA_DIR):
-        print(f"ERROR: Data folder not found at {DATA_DIR}")
+        print(f"ERROR: Data folder not available at {DATA_DIR}")
         return
 
     try:
