3a33153215185b4056b5c13dcf0bd5d88aba4f2f
srtsubham <somyar.tripathi@gmail.com>
2026-04-11 19:46:44 +0530

    Alert system pipelining with realtime chatbot tokens.

--- src/alerts.py
+++ src/alerts.py
@@ -5,8 +5,8 @@ import requests
 
 
 def send_telegram_message(message):
-    bot_token = "!#"
-    chat_id = "!#"
+    bot_token = "#"
+    chat_id = "#"
     url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
     payload = {"chat_id": chat_id, "text": message, "parse_mode": "HTML"}
     try:
