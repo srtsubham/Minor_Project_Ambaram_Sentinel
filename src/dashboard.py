3cb40f3bb3616b965445ae34a54cf0115e992a1a
srtsubham <somyar.tripathi@gmail.com>
2026-05-24 22:42:48 +0530

    Version update 7.4

--- src/dashboard.py
+++ src/dashboard.py
@@ -7,7 +7,7 @@ import zipfile
 import numpy as np
 import pandas as pd
 import pydeck as pdk
-import streamlit as st #version 7.3
+import streamlit as st #version 7.4
 import torch
 
 st.set_page_config(page_title="Ambaram Sentinel", layout="wide", page_icon="🛰️")
