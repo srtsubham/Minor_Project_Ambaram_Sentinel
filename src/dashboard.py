504cbf23d05aeb009f642bb1b775e52b70a0a516
srtsubham <somyar.tripathi@gmail.com>
2026-07-08 09:50:45 +0530

    Update streamlit dependency to 8.10

--- src/dashboard.py
+++ src/dashboard.py
@@ -7,7 +7,7 @@ import zipfile
 import numpy as np
 import pandas as pd
 import pydeck as pdk
-import streamlit as st #version 8.9
+import streamlit as st #version 8.10
 import torch
 
 st.set_page_config(page_title="Ambaram Sentinel", layout="wide", page_icon="🛰️")
