c34ce626c153623a5098b46c9413e2ddff60c8b9
srtsubham <somyar.tripathi@gmail.com>
2026-08-03 21:25:37 +0530

    Update version 10.24V. Minor bug fixes.

--- src/dashboard.py
+++ src/dashboard.py
@@ -7,7 +7,7 @@ import zipfile
 import numpy as np
 import pandas as pd
 import pydeck as pdk
-import streamlit as st #version 10.23
+import streamlit as st #version 10.24
 import torch
 
 st.set_page_config(page_title="Ambaram Sentinel", layout="wide", page_icon="🛰️")
